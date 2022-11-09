5f27e6c8a8a209e0a0dc052ac7de52e83f472a5d
whiteNgreen <adrian.r.strate@gmail.com>
2022-11-09 23:29:10 +0100

    Project

---
+++ Source/AStarPath/AStarPathGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AStarPathGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASTARPATH_API AAStarPathGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
